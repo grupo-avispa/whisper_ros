a352ee1c1ea8ad86c78c3b5c4aa867ceb35bcff2
Alberto Tudela <ajtudela@gmail.com>
2025-02-12 11:02:18 +0100

    Move to BehaviorTree 4.6
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

--- whisper_bt/include/whisper_bt/action/bt_types.hpp
+++ whisper_bt/include/whisper_bt/action/bt_types.hpp
@@ -16,18 +16,32 @@
 #ifndef WHISPER_BT__ACTION__BT_TYPES_HPP_
 #define WHISPER_BT__ACTION__BT_TYPES_HPP_
 
-#include <behaviortree_cpp_v3/behavior_tree.h>
-
 #include <string>
 
+#include "behaviortree_cpp/behavior_tree.h"
+#include "behaviortree_cpp/json_export.h"
 #include "whisper_msgs/msg/grammar_config.hpp"
 #include "whisper_msgs/msg/transcription.hpp"
+
+// Allow bi-directional convertion to JSON
+BT_JSON_CONVERTER(whisper_msgs::msg::GrammarConfig, grammar)
+{
+  add_field("grammar", &grammar.grammar);
+  add_field("start_rule", &grammar.start_rule);
+  add_field("grammar_penalty", &grammar.grammar_penalty);
+}
 
 // Template specialization to converts a string to Goal.
 namespace BT
 {
-template<> inline whisper_msgs::msg::GrammarConfig convertFromString(BT::StringView str)
+template<>
+[[nodiscard]] whisper_msgs::msg::GrammarConfig convertFromString(BT::StringView str)
 {
+  if (StartWith(str, "json:")) {
+    str.remove_prefix(5);
+    return convertFromJSON<whisper_msgs::msg::GrammarConfig>(str);
+  }
+
   whisper_msgs::msg::GrammarConfig output;
   if (!str.empty()) {
     // We expect values separated by /
@@ -43,19 +57,27 @@ template<> inline whisper_msgs::msg::GrammarConfig convertFromString(BT::StringV
   return output;
 }
 
-template<> inline whisper_msgs::msg::Transcription convertFromString(BT::StringView str)
+template<>
+[[nodiscard]] whisper_msgs::msg::Transcription convertFromString(BT::StringView str)
 {
-  // We expect real numbers separated by /
-  auto parts = splitString(str, '/');
-  if (parts.size() != 3) {
-    throw RuntimeError("invalid input)");
-  } else {
-    whisper_msgs::msg::Transcription output;
-    output.text = convertFromString<std::string>(parts[0]);
-    output.audio_time = convertFromString<float>(parts[1]);
-    output.transcription_time = convertFromString<float>(parts[2]);
-    return output;
+  if (StartWith(str, "json:")) {
+    str.remove_prefix(5);
+    return convertFromJSON<whisper_msgs::msg::Transcription>(str);
   }
+
+  whisper_msgs::msg::Transcription output;
+  if (!str.empty()) {
+    // We expect real numbers separated by /
+    auto parts = splitString(str, '/');
+    if (parts.size() != 3) {
+      throw RuntimeError("invalid input)");
+    } else {
+      output.text = convertFromString<std::string>(parts[0]);
+      output.audio_time = convertFromString<float>(parts[1]);
+      output.transcription_time = convertFromString<float>(parts[2]);
+    }
+  }
+  return output;
 }
 }  // namespace BT
 
